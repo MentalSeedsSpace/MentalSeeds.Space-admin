41def265a7f48aee67e8c8ac454ff56f13a939e7
MentalSeedsSpace <mentalseeds.space@gmail.com>
2026-09-14 15:46:47 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: 'https://snvktufzyhcyxlcickqc.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNudmt0dWZ6eWhjeXhsY2lja3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzNjc5MzcsImV4cCI6MjEwNDk0MzkzN30.dWu6FY01Wmgcp4B9iIcSy3_6ZrbenypMwk1Wt-C55SY'
+};
